c54aa2f0263bc718595a8d0e0c4d191150ab3737
unknown <sebastiandepaoli@gmail.com>
2024-12-13 19:12:54 -0300

    push21312

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0NSJI1yA9LH9qEAKpHJXi_sVBwCJb0js",
-  authDomain: "depaoli1.firebaseapp.com",
-  projectId: "depaoli1",
-  storageBucket: "depaoli1.firebasestorage.app",
-  messagingSenderId: "12772389792",
-  appId: "1:12772389792:web:ea98a7b7f05a0fd1bf2ac5"
+  apiKey: process.env.REACT_APP_API,
+  authDomain: process.env.REACT_APP_AUTH,
+  projectId: process.env.REACT_APP_PROJECT,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_MESSAGING,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
